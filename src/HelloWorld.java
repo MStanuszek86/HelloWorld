bea43f6bb5f217b85b4f446400a499e6c243b887
MStanuszek86 <58111893+MStanuszek86@users.noreply.github.com>
2019-11-23 15:33:39 +0100

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld
 {
     public static void main(String[] args)
     {
-        System.out.println("Witaj Swiecie i githubie");
+        System.out.println("Witaj Swiecie!");
+        System.out.println("Witaj GitHubie!");        
     }
 }
